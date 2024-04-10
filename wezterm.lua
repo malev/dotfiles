4ceb93b37ca85932a87fdb5de792cf5233cc7eaa
Marcos Vanetta <marcos.vanetta@nytimes.com>
2024-04-09 19:09:36 -0500

    Disable Bell

--- wezterm.lua
+++ wezterm.lua
@@ -9,6 +9,7 @@ config.enable_tab_bar = false
 config.macos_window_background_blur = 30
 config.window_background_opacity = 1
 config.window_decorations = 'RESIZE'
+config.audible_bell = "Disabled"
 
 config.mouse_bindings = {
     -- Ctrl-click will open the link under the mouse cursor
