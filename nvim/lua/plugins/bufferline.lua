90bf54889e6f59bf8da793c0783a0a58da1664d2
Marcos Vanetta <marcosvanetta@gmail.com>
2024-04-10 10:18:49 -0500

    NVim: Adding toggle terminal

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,26 @@
 return {
     "akinsho/bufferline.nvim",
     version = "*",
+    event = "VeryLazy",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
+    keys = {
+        { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+        { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+        { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+        { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+        { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+        { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    },
+    opts = {
+        options = {
+            -- stylua: ignore
+            close_command = function(n) require("mini.bufremove").delete(n, false) end,
+            -- stylua: ignore
+            right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        }
+    },
+    config = function(_, opts)
         vim.opt.termguicolors = true
-        require("bufferline").setup {}
+        require("bufferline").setup(opts)
     end
 }
