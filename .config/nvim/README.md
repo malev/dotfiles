89a06339698453c9e4d8b578e20ca23c29c05f4c
Marcos Vanetta <marcosvanetta@gmail.com>
2024-04-19 23:45:54 -0500

    Install Conform

---
+++ .config/nvim/README.md
@@ -0,0 +1,15 @@
+# NVim configuration
+
+## Debugging LSP
+
+- LSP logs can be found in `.local/state/nvim/lsp.log`
+- `:LspStart` to validate that it's running
+- `:lua vim.lsp.set_log_level 'trace'` to get more logs
+
+## Debugging CMP
+
+- `:CmpStatus`
+
+## Conform
+
+- `:ConformInfo`
---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+    'stevearc/conform.nvim',
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>cf",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            -- Conform will run multiple formatters sequentially
+            -- Use a sub-list to run only the first available formatter
+            css = { "prettier" },
+            go = { { "gofumpt", "gofmt" }, { "goimports-revise", "goimports" }, "golines" },
+            html = { "prettier" },
+            javascript = { { "prettierd", "prettier" } },
+            javascriptreact = { "prettier" },
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            terraform = { "terraform_fmt" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            ["_"] = { "trim_whitespace" },
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+    config = function(_, opts)
+        require('conform').setup(opts)
+    end
+}
