0bcee74bb9f48a3a962cfbbba1b41eb9e9e3145a
Marcos Vanetta <marcos.vanetta@nytimes.com>
2024-04-29 14:49:58 -0500

    Add copilot status to lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,13 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'AndreM222/copilot-lualine' },
     config = function()
         require('lualine').setup {
             options = {
                 theme = 'dracula'
+            },
+            sections = {
+                lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
             }
         }
     end
