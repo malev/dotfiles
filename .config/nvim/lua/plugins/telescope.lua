8647ba6481c07f061b86dc10b0fdde73db6fa484
Marcos Vanetta <marcosvanetta@gmail.com>
2024-05-03 01:09:38 -0500

    Search current word while avoiding a new plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,6 @@ return {
         tag = "0.1.6",
         dependencies = {
             { "nvim-lua/plenary.nvim" },
-            { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" }
         },
         opts = function()
             return {
