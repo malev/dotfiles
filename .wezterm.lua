07b1027c52a5ffba9c0376256c9422b19af79065
Marcos Vanetta <marcosvanetta@gmail.com>
2024-05-24 22:37:34 -0500

    Nothing much in .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,12 +12,14 @@ config.window_background_opacity = 1
 config.window_decorations = 'RESIZE'
 config.audible_bell = "Disabled"
 
+-- config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
 config.mouse_bindings = {
     -- Ctrl-click will open the link under the mouse cursor
     {
         event = { Up = { streak = 1, button = 'Left' } },
         mods = 'CTRL',
-        action = wezterm.action.OpenLinkAtMouseCursor,
+        action = act.OpenLinkAtMouseCursor,
     },
 }
 
