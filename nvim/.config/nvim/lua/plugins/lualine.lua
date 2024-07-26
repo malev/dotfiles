ccec2da105a7fef22b0bebf365c1061fdc9e8fe6
Marcos Vanetta <marcosvanetta@gmail.com>
2024-07-26 00:14:53 -0500

    Tokyo night baby

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require('lualine').setup {
             options = {
-                theme = 'dracula'
+                theme = 'tokyonight'
             },
             sections = {
                 lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
